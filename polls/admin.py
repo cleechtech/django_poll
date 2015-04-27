86cc97c5904092e5a5ba0dbe2783a6dcd5fda24b
Connor Leech <connorleech@gmail.com>
2015-04-27 10:24:55 -0700

    part 3: render polls page

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
 
+# import models from the current package
+from .models import Question, Choice
+
+class ChoiceInline(admin.TabularInline):
+	model = Choice
+	# provide enough fields for three choices
+	extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+	# Reorder fields
+	fields = ['pub_date', 'question_text']
+
+	# Group fields
+	fieldsets = []
+
+	# Choice objects are edited on the
+	# Question admin page
+	inlines = [ChoiceInline]
+
+	# fields to display as columns
+	list_display = ('question_text', 'pub_date')
+
+	# add filter sidebar
+	list_filter = ['pub_date']
+
+	# add search field
+	search_fields = ['question_text']
+
+
 # Register your models here.
+admin.site.register(Question, QuestionAdmin)
